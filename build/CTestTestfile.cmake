9106f5755c99a9d5abfe8500077b4ff097819031
Saad10A <saadrsaissi@outlook.it>
2022-05-29 22:19:02 +0200

    backup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/overcorp/LabRobotica/src
+# Build directory: /home/overcorp/LabRobotica/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lab_robotica")
---
+++ build/lab_robotica/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/overcorp/LabRobotica/src/lab_robotica
+# Build directory: /home/overcorp/LabRobotica/build/lab_robotica
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
